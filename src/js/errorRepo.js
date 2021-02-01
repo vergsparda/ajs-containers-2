4a5cf2c5793f59ee475d6fbc8be7038d057fd446
Alex Smith <alex.vesson.smith@gmail.com>
2021-02-01 20:11:44 +0300

    coverage fix

--- src/js/errorRepo.js
+++ src/js/errorRepo.js
@@ -1,10 +1,6 @@
 export default class ErrorRepository {
   constructor(arr) {
-    if (arr !== undefined) {
-      this.errors = new Map();
-    } else {
-      this.errors = new Map();
-    }
+    this.errors = new Map();
     this.errors.set(1, 'error #1');
     this.errors.set(2, 'error #2');
   }
--- src/js/errorRepo.test.js
+++ src/js/errorRepo.test.js
@@ -3,6 +3,7 @@ import ErrorRepository from './errorRepo';
 test('return current error', () => {
   const errorRepo = new ErrorRepository();
   expect(errorRepo.translate(1)).toBe('error #1');
+  expect(errorRepo.translate(2)).toBe('error #2');
 });
 
 test('return Unknown error', () => {
